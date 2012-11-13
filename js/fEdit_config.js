dd0d7a6acf950c01a6a63870e6e82094c65f7ea4
Pascal Orth <support@porth.de>
2012-11-13 22:37:32 +0100

    fixed

--- js/fEdit_config.js
+++ js/fEdit_config.js
@@ -1,8 +1,10 @@
 // fEdit Configuration
 // Verzeichnis der Dateien
 // wenn spezielle Datei angegeben, dann wird nur diese angezeigt
+
 // ist eine index-Datei enthalten, kann keine Verzeichnisausgabe erfolgen
 var filepath = 'test/test.html'; /* pfad/zu/dateien/[datei.html optional] */
+
 // Target-Script for Post-Datas
 var savecgi = 'php/savedata.php';
 var base = 'index.html' /* URL zur Ausgangsdatei */
